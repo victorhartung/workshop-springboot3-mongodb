20d9f3c2aaa2cde3d76f4103fc44d7c3b498a1e3
victorhartung <victor_hartung@hotmail.com>
2024-01-23 00:07:57 -0300

    configuration

---
+++ src/main/java/com/hartung/workshopmongo/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.hartung.workshopmongo.resources;
+
+import com.hartung.workshopmongo.domain.User;
+import com.hartung.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<User>> findAll() {
+
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+}
