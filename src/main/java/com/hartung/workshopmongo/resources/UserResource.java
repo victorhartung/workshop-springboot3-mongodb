4776d840160c2f91121a933d64c80e84ade32df0
victorhartung <victor_hartung@hotmail.com>
2024-01-24 18:04:53 -0300

    post

--- src/main/java/com/hartung/workshopmongo/resources/UserResource.java
+++ src/main/java/com/hartung/workshopmongo/resources/UserResource.java
@@ -5,10 +5,7 @@ import com.hartung.workshopmongo.dto.UserDTO;
 import com.hartung.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -45,5 +42,21 @@ public class UserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+    public ResponseEntity<UserDTO> findById(@RequestBody UserDTO objDto,  @PathVariable String id) {
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
--- src/main/java/com/hartung/workshopmongo/services/UserService.java
+++ src/main/java/com/hartung/workshopmongo/services/UserService.java
@@ -29,9 +29,26 @@ public class UserService {
         return repo.insert(obj);
     }
 
+    public void delete(String id) {
+        findById(id);
+        repo.delete(id);
+    }
+
+    public User update(User obj) {
+        User newObj = repo.findOne(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    public void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
+
 
 
 }
