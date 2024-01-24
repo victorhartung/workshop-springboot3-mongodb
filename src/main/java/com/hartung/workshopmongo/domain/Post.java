22a1177dbfd55c56e78d88609fc07898137e9ae4
victorhartung <victor_hartung@hotmail.com>
2024-01-24 20:57:40 -0300

    upd finalizando query com spring data

--- src/main/java/com/hartung/workshopmongo/domain/Post.java
+++ src/main/java/com/hartung/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.hartung.workshopmongo.domain;
 
 import com.hartung.workshopmongo.dto.AuthorDTO;
+import com.hartung.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -21,6 +24,11 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
     public Post() {
 
     }
@@ -33,6 +41,13 @@ public class Post implements Serializable {
         this.author = author;
     }
 
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
     public String getId() {
         return id;
     }
